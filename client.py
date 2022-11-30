b09458f879bf3dc5a2d97f0a626224802790917e
EkaterinaRyu <113510841+EkaterinaRyu@users.noreply.github.com>
2022-11-30 09:43:36 +0300

    borrowed from friend

--- client.py
+++ client.py
@@ -1,14 +1,94 @@
 import socket
-import pickle
+import random
+import sys
 
-HOST = '127.0.0.1'
-PORT = 8080
+import regex as re
 
-sock = socket.socket()
-sock.connect((HOST, PORT))
+alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!, '
 
-p, g, a = 7, 5, 3
-A = g ** a % p
-sock.send(pickle.dumps((p, g, A)))
+def get_keys_from_file():
+    global closed_key
+    with open('client_keys.txt', 'r') as file:
+        string = file.readline()
+        sample = re.compile(r'\d')
 
-sock.close()
+        client_open_key = int(sample.findall(string)[0])
+        server_open_key = int(sample.findall(string)[1])
+        closed_key = int(sample.findall(string)[2])
+
+        print(f"Default keys:\nClient's open key: {client_open_key}\nServer's open key: {server_open_key}\nSecret key: {closed_key}\n")
+
+def connect():
+    sock = socket.socket()
+    check = 0
+    while check == 0:
+        port = int(input('Enter the port: '))
+        while (port < 1024 or port > 65535):
+            print("Cannot connect to the port")
+            port = int(input('Enter the port: '))
+
+        try:
+            sock.connect(('127.0.0.1', port))
+            check = 1
+            return sock
+        except ConnectionRefusedError:
+            print('Cannot connect to the port, try again')
+
+def exchange_keys(sock):
+    
+    print('Connected, exchanging the keys\n')
+
+    global closed_key
+    #a = random.randint(1, 100)
+    a = 6
+    g = 5
+    p = 7
+
+    client_open_key = g ** a % p
+    sock.send(f'{str(client_open_key)};{str(g)};{str(p)}'.encode())
+
+    server_open_key = sock.recv(1024).decode()
+    try:
+        closed_key = int(server_open_key) ** a % p
+    except ValueError:
+        print('Connection to the server failed or was dropped')
+        sys.exit()
+
+    print(f"Client's open key: {client_open_key}\nServer's open key: {server_open_key}\nSecret key: {closed_key}\n")
+
+def encryption(message):
+    message_encrypted = ''
+    shift = closed_key
+
+    for symbol in message:
+        message_encrypted += alphabet[(alphabet.index(symbol) + shift) % len(alphabet)]
+
+    return message_encrypted
+
+def decryption(message):
+    message_decrypted = ''
+    shift = closed_key
+
+    for symbol in message:
+        message_decrypted += alphabet[(alphabet.index(symbol) - shift) % len(alphabet)]
+
+    return message_decrypted
+
+def exchange_messages(sock1):
+
+    print('Secure connection established\n')
+
+    message_recieved = sock.recv(1024).decode()
+    print(f'Message recieved (encoded): {message_recieved}')
+    print(f'Message recieved: {decryption(message_recieved)}\n')
+
+    message_sent = 'Hey, Server!'
+    sock.send(encryption(message_sent).encode())
+    print(f'Message sent: {message_sent}')
+    print(f'Message sent (encoded): {encryption(message_sent)}')
+
+sock = connect()
+exchange_keys(sock)
+#get_keys_from_file()
+sock1 = connect()
+exchange_messages(sock1)
--- server.py
+++ server.py
@@ -1,15 +1,94 @@
+import random
 import socket
-import pickle
+import sys
+import regex as re
 
-HOST = '127.0.0.1'
-PORT = 8080
+alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQESTUVWXYZ!, '
+certified_client_keys = [3]
+ports = [1025, 1026]
 
-sock = socket.socket()
-sock.bind((HOST, PORT))
-sock.listen(1)
-conn, addr = sock.accept()
+def connect():
+    sock = socket.socket()
+    port = int(input('Enter the port: '))
+    if (port < 1024 or port > 65535):
+        print('Cannot connect to the port')
+        port = 1025
+        while port in ports:
+            port += 1
+        ports.append(port)
+        print(f'Set to nearest free value: {port}')
 
-msg = conn.recv(1024)
-print(pickle.loads(msg))
+    sock.bind(('', port))
+    sock.listen(5)
+    print('Server is running')
+    conn, addr = sock.accept()
+    print(f'Connected: {addr}\n')
+    return conn, addr
 
-conn.close()
+def get_keys_from_file():
+    global closed_key
+    with open('server_keys.txt', 'r') as file:
+        string = file.readline()
+        sample = re.compile(r'\d')
+
+        client_open_key = int(sample.findall(string)[0])
+        server_open_key = int(sample.findall(string)[1])
+        closed_key = int(sample.findall(string)[2])
+        
+        print(f"Default keys:\nClient's open key: {client_open_key}\nServer's open key: {server_open_key}\nSecret key: {closed_key}\n")
+        
+def exchange_keys(conn1, addr1):
+    
+    global closed_key
+    b = random.randint(1, 100)
+
+    message = conn.recv(1024).decode().split(';')
+    client_open_key = message[0]
+    #if client_open_key not in certified_client_keys:
+        #print('Клиент не прошел сертификацию')
+        #conn.close()
+        #sys.exit()
+    g = message[1]
+    p = message[2]
+
+    server_open_key = int(g) ** b % int(p)
+    conn.send(str(server_open_key).encode())
+
+    closed_key = int(client_open_key) ** b % int(p)
+
+    print(f"Client's open key: {client_open_key}\nServer's open key: {server_open_key}\nSecret key: {closed_key}\n")
+
+def encryption(message):
+    message_encrypted = ''
+    shift = closed_key
+
+    for symbol in message:
+        message_encrypted += alphabet[(alphabet.index(symbol) + shift) % len(alphabet)]
+
+    return message_encrypted
+
+def decryption(message):
+    message_decrypted = ''
+    shift = closed_key
+
+    for symbol in message:
+        message_decrypted += alphabet[(alphabet.index(symbol) - shift) % len(alphabet)]
+
+    return message_decrypted
+
+def exchange_messages(conn, addr):
+
+    message_sent = 'Hello, Client!'
+    conn.send(encryption(message_sent).encode())
+    print(f'Message sent: {message_sent}')
+    print(f'Message sent (encoded): {encryption(message_sent)}\n')
+
+    message_recieved = conn.recv(1024).decode()
+    print(f'Message recieved (encoded): {message_recieved}')
+    print(f'Message recieved: {decryption(message_recieved)}')
+
+conn, addr = connect()
+exchange_keys(conn, addr)
+#get_keys_from_file()
+conn1, addr1 = connect()
+exchange_messages(conn1, addr1)
